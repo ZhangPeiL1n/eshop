0062d439fabcf6b624ca94a91d71dce379f77758
zhangpeilin <zhangpeilinn@163.com>
2022-08-09 12:37:56 +0800

    增加 PathType.java 类

---
+++ src/main/java/com/zpl/eshop/common/constant/PathType.java
@@ -0,0 +1,23 @@
+package com.zpl.eshop.common.constant;
+
+/**
+ * 文件路径类型
+ *
+ * @author ZhangPeiL1n
+ * @date 2022/8/9 12:35
+ **/
+public class PathType {
+
+    /**
+     * 相对路径
+     */
+    public static final String RELATIVE = "relative";
+
+    /**
+     * 绝对路径
+     */
+    public static final String ABSOLUTE = "absolute";
+
+    private PathType() {
+    }
+}
