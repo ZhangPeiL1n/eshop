6ba3f0f5ba82a3d0f4ba575a3025c5537e282df0
zhangpeilin <zhangpeilinn@163.com>
2022-08-16 13:38:41 +0800

    增加会员中心的查询收货地址接口

---
+++ src/main/java/com/zpl/eshop/membership/domain/DeliveryAddressDTO.java
@@ -0,0 +1,69 @@
+package com.zpl.eshop.membership.domain;
+
+import com.zpl.eshop.common.util.AbstractObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+
+/**
+ * 收货地址DTO
+ *
+ * @author ZhangPeiL1n
+ * @date 2022/8/16 12:31
+ **/
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class DeliveryAddressDTO extends AbstractObject {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 用户帐号id
+     */
+    private Long userAccountId;
+
+    /**
+     * 省份
+     */
+    private String province;
+
+    /**
+     * 市
+     */
+    private String city;
+
+    /**
+     * 区
+     */
+    private String district;
+
+    /**
+     * 收货人
+     */
+    private String consignee;
+
+    /**
+     * 收货地址
+     */
+    private String address;
+
+    /**
+     * 电话
+     */
+    private String phoneNumber;
+
+    /**
+     * 创建时间
+     */
+    private Date gmtCreate;
+
+    /**
+     * 修改时间
+     */
+    private Date gmtModified;
+
+}
