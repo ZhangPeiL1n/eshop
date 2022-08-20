c59edf6f79ecaa72e2df9746e284693e4ba30dee
zhangpeilin <zhangpeilinn@163.com>
2022-08-20 13:20:34 +0800

    增加销售出库单的注释

--- src/main/java/com/zpl/eshop/wms/domain/SaleDeliveryOrderDTO.java
+++ src/main/java/com/zpl/eshop/wms/domain/SaleDeliveryOrderDTO.java
@@ -9,6 +9,13 @@ import java.util.List;
 
 /**
  * 销售出库单DTO
+ * zpl: 这个单子是调度中心给wms(仓储)中心看的
+ * 调度中心收到订单 OrderInfoDTO(订单),创建 SaleDeliveryOrderDTO(销售出库单,实际上叫调度销售出库单更贴切)
+ * 这个单子中包括了 SendOutOrder(出库单),证明这些货物已经出库
+ * 还包括一个 LogisticOrderDTO(物流单),证明这些货物已经发货
+ * 所以流程就是 订单中心 -> OrderInfoDTO -> 调度中心 -> SaleDeliveryOrderDTO -> wms中心 -> SendOutOrder
+ * -> 物流中心 ->LogisticsOrder
+ * 这么个流程，其中创建 SaleDeliveryOrderDTO 这部分用了构造器模式 (builder，不是工厂模式)
  *
  * @author ZhangPeiL1n
  * @date 2022/1/3 17:52
