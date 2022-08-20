64551fb8272bb190572a23f6b8e6f8e6404a5cd0
zhangpeilin <zhangpeilinn@163.com>
2022-08-20 12:45:17 +0800

    增加销售出库单常量类

---
+++ src/main/java/com/zpl/eshop/wms/constant/SaleDeliveryOrderStatus.java
@@ -0,0 +1,28 @@
+package com.zpl.eshop.wms.constant;
+
+/**
+ * 销售出库单状态
+ *
+ * @author ZhangPeiL1n
+ * @date 2022/8/20 12:42
+ **/
+public class SaleDeliveryOrderStatus {
+
+    /**
+     * 编辑中
+     */
+    public static final Integer EDITING = 1;
+
+    /**
+     * 待审核
+     */
+    public static final Integer APPROVING = 2;
+
+    /**
+     * 已完成
+     */
+    public static final Integer FINISHED = 3;
+
+    private SaleDeliveryOrderStatus() {
+    }
+}
