42c0a6c79fd5257bd7b16cfef2a255edcd760475
zhangpeilin <zhangpeilinn@163.com>
2022-10-08 19:06:27 +0800

    调整财务中心接口

---
+++ src/main/java/com/zpl/eshop/finance/service/impl/FinanceServiceImpl.java
@@ -0,0 +1,52 @@
+package com.zpl.eshop.finance.service.impl;
+
+import com.zpl.eshop.finance.service.FinanceService;
+import com.zpl.eshop.wms.domain.PurchaseInputOrderDTO;
+import com.zpl.eshop.wms.domain.ReturnGoodsInputOrderDTO;
+import com.zpl.eshop.wms.domain.SaleDeliveryOrderDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 财务中心接口
+ *
+ * @author ZhangPeiL1n
+ * @date 2022/10/8 19:04
+ **/
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class FinanceServiceImpl implements FinanceService {
+
+    /**
+     * 创建采购结算单
+     *
+     * @param purchaseInputOrder 采购入库单DTO
+     * @return 处理结果
+     */
+    @Override
+    public Boolean createPurchaseSettlementOrder(PurchaseInputOrderDTO purchaseInputOrder) {
+        return true;
+    }
+
+    /**
+     * 给物流公司打款
+     *
+     * @param saleDeliveryOrder 销售出库单DTO
+     * @return 处理结果
+     */
+    @Override
+    public Boolean payForLogisticsCompany(SaleDeliveryOrderDTO saleDeliveryOrder) {
+        return true;
+    }
+
+    /**
+     * 退货打款
+     *
+     * @param returnGoodsInputOrder 退货入库单DTO
+     * @return 处理结果
+     */
+    @Override
+    public Boolean executeReturnGoodsRefund(ReturnGoodsInputOrderDTO returnGoodsInputOrder) {
+        return true;
+    }
+}
