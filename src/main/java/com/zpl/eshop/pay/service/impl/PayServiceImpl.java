340c92404d0d50d3b74a4f6bf6e9b2c734b15110
zhangpeilin <zhangpeilinn@163.com>
2022-10-29 20:17:23 +0800

    完成支付中心退款功能开发

--- src/main/java/com/zpl/eshop/pay/service/impl/PayServiceImpl.java
+++ src/main/java/com/zpl/eshop/pay/service/impl/PayServiceImpl.java
@@ -1,8 +1,15 @@
 package com.zpl.eshop.pay.service.impl;
 
 import com.zpl.eshop.order.domain.OrderInfoDTO;
+import com.zpl.eshop.pay.api.PayApi;
+import com.zpl.eshop.pay.domain.PayTransactionBuilder;
+import com.zpl.eshop.pay.domain.PayTransactionDTO;
 import com.zpl.eshop.pay.service.PayService;
+import com.zpl.eshop.pay.service.PayTransactionService;
 import com.zpl.eshop.wms.domain.ReturnGoodsInputOrderDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +21,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(rollbackFor = Exception.class)
 public class PayServiceImpl implements PayService {
 
+    private static final Logger logger = LoggerFactory.getLogger(PayServiceImpl.class);
+
+    /**
+     * 支付接口
+     */
+    @Autowired
+    private PayApi payApi;
+
+    /**
+     * 支付交易流水管理service组件
+     */
+    @Autowired
+    private PayTransactionService payTransactionService;
+
     /**
      * 获取支付二维码
      *
@@ -22,6 +43,21 @@ public class PayServiceImpl implements PayService {
      */
     @Override
     public String getQrCode(OrderInfoDTO order) {
+        try {
+            String qrCode = payApi.getQrCode(order.getPayType(),
+                    order.getOrderNo(), order.getPayableAmount());
+            ;
+
+            PayTransactionDTO payTransaction = PayTransactionBuilder.get()
+                    .buildOrderRelatedData(order)
+                    .initStatus()
+                    .create();
+            payTransactionService.save(payTransaction);
+
+            return qrCode;
+        } catch (Exception e) {
+            logger.error("error", e);
+        }
         return null;
     }
 
@@ -33,6 +69,18 @@ public class PayServiceImpl implements PayService {
      */
     @Override
     public Boolean refund(ReturnGoodsInputOrderDTO returnGoodsInputOrder) {
-        return null;
+        try {
+            PayTransactionDTO payTransaction = payTransactionService.getByOrderNo(
+                    returnGoodsInputOrder.getOrderNo());
+
+            Integer transactionChannel = payTransaction.getTransactionChannel();
+            String orderNo = returnGoodsInputOrder.getOrderNo();
+            Double refundAmount = returnGoodsInputOrder.getPayableAmount();
+
+            return payApi.refund(transactionChannel, orderNo, refundAmount);
+        } catch (Exception e) {
+            logger.error("error", e);
+            return false;
+        }
     }
 }
